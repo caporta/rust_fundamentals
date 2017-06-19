3e7a3d4d4cc2cc8651344356a2018f76908ac32e
Chris Aporta <christopher.aporta@gmail.com>
2017-06-19 08:53:23 -0400

    Add control flow structures

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -19,3 +19,51 @@ pub fn if_statement() {
             if temp > 30 { "very hot" } else { "hot" }
         } else { "ok" })
 }
+
+pub fn while_and_loop() {
+    let mut x = 1;
+
+    while x < 1000 {
+        x *= 2;
+
+        if x == 64 { continue; } // resume execution from top without running subsequent code ('skip this')
+        println!("x = {}", x);
+    }
+
+    let mut y = 1;
+    loop {  // equivalent to `while true`
+        y *= 2;
+
+        println!("y = {}", y);
+
+        if y == 1 << 10 { break; };  // jumps out of loop at 2**10
+    }
+}
+
+pub fn for_loop() {
+    for x in 1..11 { // exclusive upper bound
+        if x == 3 { continue; }
+
+        if x == 8 { break; }
+
+        println!("x = {}", x);
+    }
+
+    for (pos, y) in (30..41).enumerate() {
+        println!("{}: {}", pos, y);
+    }
+}
+
+pub fn match_statement() { // pattern matching
+    let country_code = 44; // 1..1000
+
+    let country = match country_code {
+        44 => "UK",
+        46 => "Sweden",
+        7 => "Russia",
+        1...999 => "unknown", // inclusive range notation (opposite in most langs)
+        _ => "invalid"
+    };
+
+    println!("The country with code {} is {}", country_code, country);
+}
--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,49 @@ mod control_flow;
 
 fn main() {
     println!("======================\n");
+
     println!("PRIMITIVES:\n");
     primitives::primitives();
+
     println!("\n======================\n");
+
     println!("OPERATORS:\n");
     operators::operators();
+
     println!("\n======================\n");
+
     println!("SCOPE AND SHADOWING:\n");
     scope_and_shadowing::scope_and_shadowing();
+
     println!("\n======================\n");
+
     println!("CONSTANTS:\n");
     constants::constants();
+
     println!("\n======================\n");
+
     println!("STACK AND HEAP:\n");
     stack_and_heap::stack_and_heap();
+
     println!("\n======================\n");
-    println!("CONTROL FLOW:\n");
+
+    println!("IF STATEMENT:\n");
     control_flow::if_statement();
+
+    println!("\n======================\n");
+
+    println!("WHILE AND LOOP:\n");
+    control_flow::while_and_loop();
+
+    println!("\n======================\n");
+
+    println!("FOR LOOP:\n");
+    control_flow::for_loop();
+
+    println!("\n======================\n");
+
+    println!("MATCH STATEMENT:\n");
+    control_flow::match_statement();
+
     println!("\n======================\n");
 }
